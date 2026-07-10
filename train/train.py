2d180a1aa4aca00d11b2f20d0cd0b8491fe0f459
전정훈 <jjhoon7663@neurald.com>
2026-07-10 15:53:02 +0900

    Add training skeleton

---
+++ train/train.py
@@ -0,0 +1,21 @@
+# train/train.py
+
+from models.clip import CLIPModel
+
+
+def train():
+
+    print("========== Train Start ==========")
+
+    # TODO
+    # Dataset Load
+    # DataLoader
+    # Loss
+    # Optimizer
+    # Epoch
+
+    print("Training...")
+
+
+if __name__ == "__main__":
+    train()
